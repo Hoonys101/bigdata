e81d269f429f284db2b7fa612cfb840cfd472352
taengal-test <taengal@gmail.com>
2024-04-08 16:47:58 +0900

    interface와 javapython 구현객체 만들었으나 안돌아감

---
+++ src/main/java/team/backend/service/JavaPyThonInter.java
@@ -0,0 +1,19 @@
+package team.backend.service;
+
+public interface JavaPythonInter {
+    boolean strParameter(String... args);
+    /*첫번째 인자가 "add_data"인 경우
+      "add_data", "db_name", "stock_code"를 인자로 호출
+      첫번째 인자가 "cal_data"인 경우
+      "cal_data",
+      "first_stock_code","first_db_name",
+      "second_stock_code","second_db_name",
+      "startdate","lastdate"를 인자로 호출
+      첫번째 인자가 "diff_call_data"인 경우
+      "diff_call_data",
+      "first_stock_code","first_db_name",
+      "diff_stock_code","diff_db_name",
+      "second_stock_code","second_db_name",
+      "startdate","lastdate"를 인자로 호출
+     */
+}
